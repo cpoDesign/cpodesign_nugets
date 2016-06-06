bad4fd735e08a330d84ee9f3da952a5bed7badd4
psvarc <pvl.svarc@gmail.com>
2016-06-06 13:54:42 +0100

    updated

---
+++ CpoDesign_nugets/CpoDesign_nugets/TextManipulation/TextManupulation.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CpoDesign_nugets.TextManipulation
+{
+    public class TextManupulation
+    {
+        public static string RemoveTextInBracketsForText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return text;
+            }
+
+            string regex = "(\".*\")|('.*')|(\\(.*\\))";
+            string output = Regex.Replace(text, regex, string.Empty);
+            return output;
+        }
+    }
+}
